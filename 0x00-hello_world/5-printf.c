c622dc45d3d6c7041a4dd830161dd6702dfd1b13
dolapo <omobitandolapo@gmail.com>
2023-10-05 10:57:31 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - program entry point
+*
+* Description: Prints "with proper grammar, but the outcome
+* is a piece of art."
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

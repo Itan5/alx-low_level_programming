16315380ef8a24efdcd5941bbda82173a7a92aa6
dolapo <omobitandolapo@gmail.com>
2023-10-06 15:12:55 -0700

    all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ *Description: Prints "all single digit numbers of base 10 starting from 0"
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int sinint;
+
+	sinint = 0;
+
+	while
+		(sinint <= 9)
+
+	{
+		printf("%d", sinint);
+		sinint++;
+	}
+
+		printf("\n");
+	return (0);
+}
+

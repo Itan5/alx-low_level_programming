a5664f6f6cb449c729533304876022844a49026d
dolapo <omobitandolapo@gmail.com>
2023-10-06 15:27:59 -0700

    numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ *Description: Prints "all single digit numbers of base 10 starting from 0"
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int sinint;
+
+	sinint = 0;
+
+	while
+		(sinint <= 9)
+
+	{
+		putchar('0' + sinint);
+		sinint++;
+	}
+	{
+
+		putchar('\n');
+	}
+	return (0);
+}
+

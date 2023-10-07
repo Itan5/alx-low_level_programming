71367913a8c487e190e103b191277335674f1c13
dolapo <omobitandolapo@gmail.com>
2023-10-07 11:01:44 -0700

    combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ *Description: Prints "all possible combinations of single-digit numbers"
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	int pdd[50];
+
+	int ppd[50];
+
+	pdd[0] = 0;
+
+	while
+		(pdd[0] < 99)
+	{
+		ppd[0] = 0;
+	while
+		(ppd[0] < 100)
+	{
+		if (pdd < ppd)
+	{
+		putchar('0' + pdd[0] / 10);
+
+		putchar('0' + pdd[0] % 10);
+
+		putchar(' ');
+
+		putchar('0' + ppd[0] / 10);
+		putchar('0' + ppd[0] % 10);
+
+	if (pdd[0] != 98 || ppd[0] != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+
+	ppd[0]++;
+	}
+	pdd[0]++;
+	}
+
+		putchar('\n');
+	return (0);
+}
+

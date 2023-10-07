b31e79088bd97ff0582124ae398191f246582e0b
dolapo <omobitandolapo@gmail.com>
2023-10-06 17:13:59 -0700

    comma seperated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,17 @@ int main(void)
 
 	int comint[50];
 
-	comint[0] = 0;
+	comint[0] = 10;
 
 	while
-		(comint[0] <= 9)
+		(comint[0] <= 99)
 	{
-		putchar('0' + comint[0]);
+		putchar('0' + comint[0] / 10);
+		putchar('0' + comint[0] % 10);
 		comint[0]++;
-		if (comint[0] <= 9)
+		if (comint[0] <= 99)
 	{
 		putchar(',');
-		putchar(' ');
 	}
 	}
 

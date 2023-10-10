6244428d12acc9b807182ea61a4bd2a0ea0f5d9a
dolapo <omobitandolapo@gmail.com>
2023-10-10 10:31:47 -0700

    alphabet check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point of program
+ *
+ *Description: Prints "1 if it is an alphabet and 0 if not"
+ *
+ *@c: contains all character
+ * Return: Always 0 (success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1)
+
+	else if (c >= 'A' && c <= 'Z')
+	return (1)
+
+	else
+	return (0)
+}

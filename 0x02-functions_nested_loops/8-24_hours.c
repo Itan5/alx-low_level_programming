f92d00d01437f177ffe8d09146f61fc9d8b3b4d7
dolapo <omobitandolapo@gmail.com>
2023-10-10 13:32:24 -0700

    time of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point of program
+ *
+ *Description: Prints "all time of the day"
+ *
+ *
+ * Return: Always 0 (success)
+ */
+void jack_bauer(void)
+{
+	int hrs;
+
+	int mins;
+
+	int th;
+
+	int tih;
+
+	int tm;
+
+	int tim;
+
+	hrs = 0;
+
+	while
+	(hrs <= 23)
+	{
+	mins = 0;
+
+	th = hrs / 10;
+	tih = hrs % 10;
+
+	while
+	(mins <= 59)
+	{
+	tm = mins / 10;
+	tim = mins % 10;
+	_putchar('0' + th);
+	_putchar('0' + tih);
+	_putchar(':');
+	_putchar('0' + tm);
+	_putchar('0' + tim);
+	_putchar('\n');
+	min++;
+	}
+	hrs++;
+	}
+}

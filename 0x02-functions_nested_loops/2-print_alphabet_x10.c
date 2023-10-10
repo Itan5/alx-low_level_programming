9af2af10fcc5648a0b81ac2145b8aad3ba94da75
dolapo <omobitandolapo@gmail.com>
2023-10-10 09:25:35 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Input point
+ *
+ *Description: Prints "all alphabets in lower casein 10 places"
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char alph = 'a';
+
+	for (dp = 0; dp < 10; dp++)
+	{
+		while
+		(alph <= 'z')
+		{
+		_putchar(alph);
+		alph++;
+		}
+		_putchar('\n');
+	}
+}

88085a194620c3bb2d348024eb53786357e44a46
dolapo <omobitandolapo@gmail.com>
2023-10-10 15:48:44 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/.9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * times_table - Entry point of program
+ *
+ *Description: Prints "multiplication table between 0 and 9"
+ *
+ *
+ * Return: Always 0 (success)
+ */
+void times_table(void)
+{
+	int m, t;
+	int tt, x, y;
+
+	m = 0;
+	while
+	(m <= 9)
+	{
+	t = 0;
+	while
+	(t <= 9)
+	{
+	tt = m * t;
+	if (m < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	if  (tt < 10)
+	{
+	_putchar(' ');
+	}
+	}
+	if  (tt < 10)
+	{
+	_putchar('0' + tt);
+	}
+	else
+	{
+	x = tt / 10;
+	y = tt % 10;
+	_putchar('0' + x);
+	_putchar('0' + y);
+	}
+	}
+	}
+}

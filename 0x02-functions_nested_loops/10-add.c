dc5480efc14f0b8ea49dd0251906272909c3aef0
dolapo <omobitandolapo@gmail.com>
2023-10-10 16:40:36 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point of program
+ *
+ *Description: Prints "addition of numbers"
+ *
+ *@d: first numbers to be added
+ *@p: second number to be added
+ *
+ * Return: Always 0 (success)
+ */
+int add(int d, int p);
+{
+return (d + P);
+}

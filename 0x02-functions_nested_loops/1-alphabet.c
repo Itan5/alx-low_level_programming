8e7632f5f20c928886b4203d67c5feebcef3387a
dolapo <omobitandolapo@gmail.com>
2023-10-10 08:43:45 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Input point of program
+ *
+ *Description: Prints "_putchar"
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	char alph = 'a';
+
+	while
+	(a <= z)
+	{
+	_putchar(alph);
+	alph++
+	}
+	_putchar('\n');
+
+	return (0);
+}

23a1868d1f6ac1651bd22aa1c7e755d02c3832c9
dolapo <omobitandolapo@gmail.com>
2023-10-10 11:57:46 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Entry point of program
+ *
+ *Description: Prints "absolute value of an integer"
+ *
+ *@d: contains the number
+ * Return: Always 0 (success)
+ */
+int _abs(int d)
+{
+	if (d > 0)
+	return (d);
+
+	else if (d < 0)
+	return (d);
+
+	else
+	return (0);
+}

381b6f1b66cb4cf4649bed5d09ce6125fe5521df
dolapo <omobitandolapo@gmail.com>
2023-10-10 10:10:57 -0700

    case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _islower - Entry point of program
+ *
+ *Description: Prints "1 if an alphabet is lower case and 0 if not"
+ *
+ * Return: Always 0 (success)
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+	_putchar('1');
+	_putchar('\n');
+	}
+	
+	else 
+	{
+	_putchar('0');
+	}
+}

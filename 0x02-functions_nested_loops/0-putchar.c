119f8566399ede178f96ce14361cdb5dc939e9f4
dolapo <omobitandolapo@gmail.com>
2023-10-10 05:21:40 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Input point of program
+ *
+ *Description: Prints "_putchar"
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar("\_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this repo contains all tasks related to ax 0x02. C - Functions, nested loops

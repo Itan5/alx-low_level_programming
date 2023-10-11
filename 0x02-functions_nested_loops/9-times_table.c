d9260fdde6d272796154b802cff39a52e1431ad5
dolapo <omobitandolapo@gmail.com>
2023-10-10 17:19:29 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,45 +1,39 @@
 #include "main.h"
+
 /**
  * times_table - Entry point of program
  *
- *Description: Prints "all timetable between 0 and 9"
- *
+ * Description: Prints the times table between 0 and 9 using while loops.
  *
  * Return: Always 0 (success)
  */
 void times_table(void)
 {
-	int h;
-	int m;
-	int tt;
-	int x;
-	int y;
+	int h, m, tt;
 
 	h = 0;
-	while
-	(h <= 9)
+	while (h <= 9)
 	{
 	m = 0;
-	while
-	(m <= 9)
+	while (m <= 9)
 	{
 	tt = h * m;
-	if (tt < 10 && m > 0)
+
+	if (m > 0)
 	{
-	_putchar('0' + tt);
 	_putchar(',');
-	_putchar(' ');
 	_putchar(' ');
 	}
-	else if
-	(tt >= 10)
+
+	if (tt < 10)
 	{
-	x = tt / 10;
-	y = tt % 10;
-	_putchar('0' + x);
-	_putchar('0' + y);
-	_putchar(',');
 	_putchar(' ');
+	_putchar('0' + tt);
+	}
+	else
+	{
+	_putchar('0' + (tt / 10));
+	_putchar('0' + (tt % 10));
 	}
 	m++;
 	}

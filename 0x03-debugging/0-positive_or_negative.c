8a555d31fd6508f4bc8a512e4c48f38e65b7633a
dolapo <omobitandolapo@gmail.com>
2023-10-11 14:51:10 -0700

    jvjkvdfopkfd

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point of program
+ *
+ *Description: Prints "the psoitive and negative status of random numbers"
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+
+		printf("%d is negative\n", n);
+
+	else if (n > 0)
+
+		printf("%d is positive\n", n);
+
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}

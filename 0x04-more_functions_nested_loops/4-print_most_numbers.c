1ce22de9738d6d7d3db34b9d0f3da9fafaf07de5
dolapo <omobitandolapo@gmail.com>
2023-10-12 05:46:22 -0700

    numbers without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int d = 0;
+
+	while
+		(d <= 9)
+		{
+		if (d != 2 && d != 4)
+			{
+			_putchar('0' + d);
+			}
+		d++;
+		}
+	_putchar('\n');
+}
+

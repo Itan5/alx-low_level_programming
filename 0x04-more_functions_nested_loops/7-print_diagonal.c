0996e158de374c93a010a02fc7b7dfe21bf7a529
dolapo <omobitandolapo@gmail.com>
2023-10-12 08:42:36 -0700

    function that draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal.
+ *
+ * @n: Length of the diagonal line
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int d = 0;
+	int dp;
+
+	while (d < n)
+	{
+		dp = 0;
+		while (dp < d)
+		{
+			if (n > 0)
+			{
+				_putchar(' ');
+			}
+			dp++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		d++;
+	}
+}
+

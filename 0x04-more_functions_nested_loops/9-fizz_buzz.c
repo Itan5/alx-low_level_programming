1fc95af6a7d7c34798875bcea2f96cd0cfe36c0d
dolapo <omobitandolapo@gmail.com>
2023-10-12 09:59:24 -0700

    prints the numbers from 1 to 100 with fizzbuzz replacement

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: prints the numbers from 1 to 100, with some fizzbuzz
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 1;
+
+	while
+	(a <= 100)
+	{
+	if ((a % 15) == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if ((a % 5) == 0)
+	{
+	printf("Buzz");
+	}
+	else if ((a % 3) == 0)
+	{
+	printf("Fizz");
+	}
+	else
+	{
+	printf("%d ", a);
+	}
+	if (a != 100)
+	printf(" ");
+	a++;
+	}
+	printf("\n");
+return (0);
+}

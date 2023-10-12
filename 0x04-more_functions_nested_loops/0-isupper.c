5bfcf5e74092586c9d7d0c23f69cb4f288293487
dolapo <omobitandolapo@gmail.com>
2023-10-12 04:23:51 -0700

    is digit and is alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - return 1 if upper and return 0 if lower
+ *
+ *@c: alphabet to be check
+ * Return: Always 0 (successfull)
+ */
+int _isupper(int c)
+{
+	while 
+		(c >= 65 && c <= 90)
+		return (1);
+	while
+		(c < 65 && c > 90)
+		return (0);
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - Checks if an input is a digit or not
+ *
+ * @c: Inpute to be checked
+ * Return: Always 0 (successful).
+ */
+int _isdigit(int c)
+{
+	while
+		(c >= 48 && c <= 57)
+			return (1);
+	while
+		(c < 48 && c > 57)
+			return (0);
+return (0);
+}

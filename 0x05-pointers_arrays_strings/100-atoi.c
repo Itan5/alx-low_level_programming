1685b5d50b5fa55f2ab537a4efa8edb0590e80d4
dolapo <omobitandolapo@gmail.com>
2023-10-17 10:25:46 -0700

    a function that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*_atoi - convert a string to an integer
+*@s: string to be printed
+*Return: (num *sign)
+*/
+int _atoi(char *s)
+{
+	unsigned int dp = 0;
+	int num = 0;
+	int sign = 1;
+
+	while (s[dp] != '\0' && (s[dp] < '0' || s[dp] > '9'))
+	{
+		if (s[dp] == '-')
+		{
+			sign *= -1;
+		}
+		dp++;
+	}
+
+	while (s[dp] != '\0' && (s[dp] >= '0' && s[dp] <= '9'))
+	{
+		num = (num * 10 + (s[dp] - '0'));
+		dp++;
+	}
+
+	return (num * sign);
+}
+
